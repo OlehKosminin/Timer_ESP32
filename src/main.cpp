04aa2219f355f210d9e0465d12c63398cafb787f
Oleh <arzamazova13@gmail.com>
2024-02-09 21:55:57 +0200

    add timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,59 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define btnStart 22
+#define btnStop 23
+unsigned long startTime = 0;
+unsigned long elapsedTime = 0;
+bool timerRunning = false;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(btnStart, INPUT_PULLUP);
+  pinMode(btnStop, INPUT_PULLUP);
+  startTime = millis();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  if (digitalRead(btnStart) == LOW)
+  {
+    timerRunning = true;
+    Serial.print("Timer state changed to ");
+    Serial.println("running");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (timerRunning)
+    {
+      startTime = millis() - elapsedTime;
+    }
+  }
+  if (digitalRead(btnStop) == LOW)
+  {
+    timerRunning = false;
+    Serial.print("Timer state changed to ");
+    Serial.println("stopped");
+  }
+  delay(50);
+
+  if (timerRunning)
+  {
+    unsigned long currentTime = millis();
+    elapsedTime = currentTime - startTime;
+
+    unsigned long hours = (elapsedTime / 3600000) % 24;
+    unsigned long minutes = (elapsedTime / 60000) % 60;
+    unsigned long seconds = (elapsedTime / 1000) % 60;
+
+    Serial.print(hours < 10 ? "0" : "");
+    Serial.print(hours);
+    Serial.print(":");
+    Serial.print(minutes < 10 ? "0" : "");
+    Serial.print(minutes);
+    Serial.print(":");
+    Serial.print(seconds < 10 ? "0" : "");
+    Serial.print(seconds);
+    Serial.println("");
+  }
+
+  delay(1000);
 }
